37cf0ad6288d68ed01d35db943afc061a8f4cd34
krzbar <bartoszekkj@gmail.com>
2024-12-19 20:38:38 +0100

    Corrected errors
    added Nnode, tip.labels update
    commented code

--- add_cherry_tip.R
+++ add_cherry_tip.R
@@ -1,58 +1,31 @@
+## These are the R scripts and numerical results accompanying 
+## CHECK FINAL AUTHORS
+## Bartoszek, Brahmantio, Munoz-Duran, Fuentes-Gonzalez, Chi Kiang, Pienaar, and Polly
+## TITLE?
+## "TITLE?"
+
+## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
+## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+## Please understand that there may still be bugs and errors. Use it at your own risk. 
+## We take no responsibility for any errors or omissions in this code or for any misfortune 
+## that may befall you or others as a result of its use. Please send comments and report 
+## bugs to Krzysztof Bartoszek at krzbar@protonmail.ch .
 add_new_tip_split<-function(phyltree,sister_tip,new_branch_len){
     new_bl<-new_branch_len
-    n<-max(phyltree$edge)-phyltree$Nnode
+    n<-max(phyltree$edge)-phyltree$Nnode ## number of tips
     if (sister_tip>n){stop(paste0("sister_tip is not a tip node, maximum possible is: ",n))}
-    b_id<-which(phyltree$edge[,2]==sister_tip)
+    b_id<-which(phyltree$edge[,2]==sister_tip) ## id branch to split
     if(new_bl<phyltree$edge.length[b_id]){
-	new_id<-max(phyltree$edge)+2	
-	phyltree$edge<-phyltree$edge+1
-	anc_id<-phyltree$edge[b_id,1]
-	phyltree$edge[b_id,]<-c(new_id,sister_tip+1)
-	phyltree$edge<-rbind(phyltree$edge,rbind(c(new_id,1),c(anc_id,new_id)))
-	phyltree$edge.length<-c(phyltree$edge.length,new_bl,phyltree$edge.length[b_id]-new_bl)
-	phyltree$edge.length[b_id]<-new_bl
+	new_id<-max(phyltree$edge)+2 ## new tip node id
+	phyltree$edge<-phyltree$edge+1 ## increase all current node ids by 1 as new tip will be added
+	anc_id<-phyltree$edge[b_id,1] ## node id of direct ancestor of sister_tip
+	phyltree$edge[b_id,]<-c(new_id,sister_tip+1) ## tip branch of sister_tip, entries of edge update sister_tip not
+	phyltree$edge<-rbind(phyltree$edge,rbind(c(new_id,1),c(anc_id,new_id))) ## create edge leading to new tip and edge leading to its direct ancestor, i.e., split tip edge of sister_tip
+	phyltree$edge.length<-c(phyltree$edge.length,new_bl,phyltree$edge.length[b_id]-new_bl) ## add lengths of new edges
+	phyltree$edge.length[b_id]<-new_bl ## update length of tip edge of sister_tip
+	phyltree$tip.label<-c(paste0("t",n+1),phyltree$tip.label) ## update tip labels
+	phyltree$Nnode<-phyltree$Nnode+1 ## update number of internal nodes 
     }else{stop(paste0("New branch length is too long, has to be shorter than: ",new_bl))}
     phyltree
 }
 
-Need to find error in below
-> phyltree<-sim.bd.taxa(3,1,1,0)[[1]]
-> newtree<-add_new_tip_split(phyltree,2,0.01)
-Error in add_new_tip_split(phyltree, 2, 0.01) : 
-  object 'phytree' not found
-> source("add_cherry_tip.R")
-> newtree<-add_new_tip_split(phyltree,2,0.01)
-Error in phyltree$edge[b_id, 1] <- c(new_id, sister_tip) : 
-  number of items to replace is not a multiple of replacement length
-> source("add_cherry_tip.R")
-> newtree<-add_new_tip_split(phyltree,2,0.01)
-> plot(newtree)
-Error in plot.phylo(newtree) : 
-  tree badly conformed; cannot plot. Check the edge matrix.
-> phyltree$edge
-     [,1] [,2]
-[1,]    4    5
-[2,]    5    2
-[3,]    5    1
-[4,]    4    3
-> newtree$edge
-     [,1] [,2]
-[1,]    5    6
-[2,]    7    2
-[3,]    6    2
-[4,]    5    4
-[5,]    7    1
-[6,]    6    7
-> source("add_cherry_tip.R")
-> newtree<-add_new_tip_split(phyltree,2,0.01)
-> plot(newtree)
-Error in plot.phylo(newtree) : 
-  tree badly conformed; cannot plot. Check the edge matrix.
-> newtree$edge
-     [,1] [,2]
-[1,]    5    6
-[2,]    7    3
-[3,]    6    2
-[4,]    5    4
-[5,]    7    1
-[6,]    6    7
