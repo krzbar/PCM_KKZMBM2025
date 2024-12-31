0d52f63eee3a88536a8b5d7b86c190714c95dfd1
krzbar <bartoszekkj@gmail.com>
2024-12-31 16:20:27 +0100

    example comparing BM in mvSLOUCH and mvMORPH

--- mvSLOUCHversusmvMORPHestimation.R
+++ mvSLOUCHversusmvMORPHestimation.R
@@ -19,14 +19,46 @@
 ## comment that convergence attained. mvSLOUCH returns -Inf for likelihood prompting the user to hopefully
 ## investigate the tree
 
-phyltree50<-sim.bd.taxa(50,1,1,0)[[1]]
-plot(phyltree50)
-which(phyltree50$tip.label=="t50") ## here we perhaps need to change the node dependen on the random seed
-phyltree51<-add_new_tip_split(phyltree,4,0.00001)
-BMdata51<-simulBMProcPhylTree(phyltree51, X0=matrix(c(0,0),ncol=1), Sigma=diag(1,2,2))
-BMdata50<-BMdata[-1,]
-resmvsl50<-BrownianMotionModel(phyltree50,BMdata50);resmvsl50$ParamSummary$LogLik;resmvsl50$ParamsInModel$vX0;resmvsl50$ParamSummary$StS
-resmvsl51<-BrownianMotionModel(phyltree51,BMdata51);resmvsl51$ParamSummary$LogLik;resmvsl51$ParamsInModel$vX0;resmvsl51$ParamSummary$StS
-resmvmorBM150<-mvMORPH::mvBM(phyltree50,BMdata50, model="BM1", scale.height = FALSE) ## notice the big difference betweem the two estimates
-resmvmorBM151<-mvMORPH::mvBM(phyltree51,BMdata51, model="BM1", scale.height = FALSE) ## perhaps saying something very different
+library(ape)
+library(mvSLOUCH)
+library(TreeSim)
+library(mvMORPH)
+source("add_cherry_tip.R")
 
+rexp(1)
+n<-30
+rm(.Random.seed)
+my_random_seed<-1.890968652176264530596
+#my_random_seed<-rexp(1)
+set.seed(my_random_seed)
+phyltreen<-TreeSim::sim.bd.taxa(n,1,1,0)[[1]]
+#plot(phyltreen)
+
+#tip_id<-which(phyltreen$tip.label=="t40") # 1000
+tip_label<-"t15"
+#tip_label<-paste0("t",sample(1:n,1))
+tip_id<-which(phyltreen$tip.label==tip_label) #54321
+phyltreen1<-add_new_tip_split(phyltreen,tip_id,0.000001)
+BMdatan1<-mvSLOUCH::simulBMProcPhylTree(phyltreen1, X0=matrix(c(10,0),ncol=1), Sigma=rbind(c(0.1,0),c(-10,10)))
+BMdatan<-BMdatan1[-1,]
+resmvsln1<-mvSLOUCH::BrownianMotionModel(phyltreen1,BMdatan1);resmvsln1$ParamSummary$LogLik;resmvsln1$ParamsInModel$vX0;resmvsln1$ParamSummary$StS
+resmvsln<-mvSLOUCH::BrownianMotionModel(phyltreen,BMdatan);resmvsln$ParamSummary$LogLik;resmvsln$ParamsInModel$vX0;resmvsln$ParamSummary$StS
+resmvmorBM1n1<-mvMORPH::mvBM(phyltreen1,BMdatan1, model="BM1", scale.height = FALSE) 
+resmvmorBM1n<-mvMORPH::mvBM(phyltreen,BMdatan, model="BM1", scale.height = FALSE) 
+
+
+print("Original tree and data, mvSLOUCH raises error.")
+print(resmvsln1)
+print("==========================================")
+
+print("Tree and data with short tip branch species removed, mvSLOUCH estimates without error.")
+print(resmvsln)
+print("==========================================")
+
+print("Tree and data with short tip branch species removed, mvMORPH estimates same as mvSLOUCH.")
+print(resmvmorBM1n)
+print("==========================================")
+
+print("Original tree and data, mvMORPH returns different estimates.")
+print(resmvmorBM1n1)
+print("==========================================")
