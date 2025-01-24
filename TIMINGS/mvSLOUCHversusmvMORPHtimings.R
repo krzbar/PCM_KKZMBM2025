f50ed9bd143d815c922a592d8c51ddcdd3b02dd3
krzbar <bartoszekkj@gmail.com>
2025-01-24 14:53:54 +0000

    Update mvSLOUCHversusmvMORPHtimings.R
    updated for running

--- TIMINGS/mvSLOUCHversusmvMORPHtimings.R
+++ TIMINGS/mvSLOUCHversusmvMORPHtimings.R
@@ -19,14 +19,14 @@ library(mvSLOUCH)
 library(TreeSim)
 library(mvMORPH)
 
-v_doneN<-c(5,10,30,50,100,200,500,1000) ##2000,5000,10000,20000,50000)
-vN<-c(1500,2000)
+v_doneN<-c() 
+vN<-c(5,10,30,50,100,200,500,1000,1500,2000)
 numreps<-30
 BMparams<-list(X0=matrix(0,ncol=1,nrow=4),Sigma=rbind(c(1,0,0,0),c(0.5,1,0,0),c(0.5,0.5,1,0),c(0.5,0.5,0.5,1)))
 filepath<-"."
 fileprefix<-"mvSLOUCHmvMORPH_BM_timings"
 vcolours<-c("red","blue","orange")
-b_dosimulation<-FALSE
+b_dosimulation<-TRUE
 c_plotlogscale<-TRUE
 
 if (b_dosimulation){
