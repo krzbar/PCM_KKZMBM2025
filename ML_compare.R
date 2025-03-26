fbe07c5df532baf3819e7bd9dc9220f3ee9523a8
bayubeta <bayubrahmantio@gmail.com>
2025-03-26 14:12:40 +0100

    Added files for simulation study

---
+++ ML_compare.R
@@ -0,0 +1,335 @@
+# ------ Calculate maximum likelihood given trees with short branch tips -------
+# two cases:
+# - small tree: n = 4
+# - large tree: n = 100
+library(ape)
+library(TreeSim)
+library(mvSLOUCH)
+library(mvMORPH)
+
+# random seed
+random_seeds <- 123 + 0:2
+
+# =============== create a tree with 4 tips and simulate data ==================
+tree_4 <- read.tree(text = "(y1, ((y2, y3), y4));") 
+tree_4$edge.length <- c(1, 0.5, 0.25, 0.25, 0.25, 0.5)
+
+# ----------- new tree (for plotting)
+# make the tip branch shorter
+tree_new <- tree_4
+l_new <- 0.245
+tree_new$edge.length <- c(1, 0.5, 0.25+l_new, 0.25-l_new, 0.25-l_new, 0.5)
+
+pdf("four_tips.pdf", width = 8, height = 4)
+par(mfrow = c(1,2))
+plot.phylo(tree_4, main = "A", label.offset = 0.1, edge.width = 2)
+axis(1, at = seq(0, 1, by = 0.2))
+plot.phylo(tree_new,  main = "B", label.offset = 0.1, edge.width = 2)
+axis(1, at = seq(0, 1, by = 0.2))
+dev.off()
+
+# --------------------- simulate traits at tips
+set.seed(random_seeds[1])
+X0 <- matrix(c(0, 0), ncol = 1) # ancestral trait value at the root
+Sigma <- matrix(c(1,2,2,1), nrow = 2)
+StS <- Sigma %*% t(Sigma)
+
+# vector of true parameter values
+truevalues <- c(X0, c(StS)[c(1,2,4)])
+
+# simulate
+Xsim_4 <- mvSLOUCH::simulBMProcPhylTree(phyltree = tree_4, X0 = X0, Sigma = Sigma)
+
+# ============= create a tree with 100 tips and simulate data ==================
+set.seed(random_seeds[2])
+tree_100 <- TreeSim::sim.bd.taxa(n = 100, numbsim = 1, lambda = 1, mu = 0)[[1]] 
+# normalize
+tree_100$edge.length <- tree_100$edge.length/max(nodeHeights(tree_100))
+
+# --------------------- simulate traits at tips
+set.seed(random_seeds[3])
+# simulate
+Xsim_100 <- mvSLOUCH::simulBMProcPhylTree(phyltree = tree_100, X0 = X0, Sigma = Sigma)
+
+# find the shortest tip branches in the tree
+get_short_tip <- function(tree){
+  # find which tip branch is the shortest
+  tip_id <- which(tree$edge[,2] %in% 1:Ntip(tree))
+  edge_id <- tip_id[which.min(tree$edge.length[tip_id])]
+  parent_node <- tree$edge[edge_id,1]
+  ids_desc <- which(tree$edge[,1] == parent_node) # ids of tip branch
+  id_anc <- which(tree$edge[,2] == parent_node) # id of ancestor
+  return(c(id_anc, ids_desc))
+}
+
+
+# ======================= sequence of tip branch length ========================
+n_seq <- 100
+
+# for tree_4
+l_seq_4 <- sapply(1:n_seq, function(n){0.25*((2/3)^(n-1))})
+
+# for tree_100, subset of l_seq_4 which is lower than l_stip
+l_stip <- tree_100$edge.length[get_short_tip(tree_100)[2]]
+l_seq_100 <- c(l_stip, l_seq_4[l_seq_4 < l_stip])
+
+
+# =============== ML functions given tree and branch length  ===================
+# retrieve maximum likelihood parameters given
+# branch length and simulated data
+ml_params <- function(l, tree, Xsim, method = c("mvSLOUCH", "rpf", "pic")){
+  if (length(method) == 2){
+    method <- "mvSLOUCH"
+    cat("mvSLOUCH is used \n")
+  }
+  
+  # number of tips
+  ntips <- Ntip(tree)
+  
+  # change the tip branch length
+  if (ntips == 4){
+    tree_new <- tree
+    tree_new$edge.length[c(4,5)] <- l
+    tree_new$edge.length[3] <- 0.5 - l
+  }else{
+    tree_new <- tree
+    ids_edges <- get_short_tip(tree_new)
+    l_anc_edge <- tree_new$edge.length[ids_edges[1]]
+    tree_new$edge.length[ids_edges[2:3]] <- l
+    tree_new$edge.length[ids_edges[1]] <- l_anc_edge - l
+  }
+  
+  
+  # return a vector: c(x01, x02, StS11, StS12, StS22)
+  if (method == "mvSLOUCH"){
+    res <- mvSLOUCH::BrownianMotionModel(phyltree = tree_new, mData = Xsim)
+    if (is.infinite(res$ParamSummary$LogLik)){
+      return(rep(NA, 6))
+    }else{
+      return(c(c(res$ParamsInModel$vX0), c(res$ParamSummary$StS)[c(1,2,4)],
+               res$ParamSummary$LogLik))
+    }
+    
+  }else{
+    if (method %in% c("rpf", "pic")){
+      res <- try(mvMORPH::mvBM(tree = tree_new, data = Xsim, method = method, 
+                               echo = FALSE), silent = TRUE)
+      if ("try-error" %in% class(res)){
+        return(rep(NA, 6))
+      }else{
+        return(c(res$theta, res$sigma[c(1,2,4)], res$LogLik))
+      }
+    }
+  }
+}
+
+
+# ================== run ML inference for tree with n = 4 ======================
+
+# mvSLOUCH
+mlparams_mvs_4 <- sapply(l_seq_4, ml_params, tree = tree_4, Xsim = Xsim_4, 
+                         method = "mvSLOUCH")
+
+# rpf (mvMORPH)
+mlparams_rpf_4 <- sapply(l_seq_4, ml_params, tree = tree_4, Xsim = Xsim_4, 
+                         method = "rpf")
+
+# pic (mvMORPH)
+mlparams_pic_4 <- sapply(l_seq_4, ml_params, tree = tree_4, Xsim = Xsim_4, 
+                         method = "pic")
+
+
+# ================= run ML inference for tree with n = 100 =====================
+
+# mvSLOUCH
+mlparams_mvs_100 <- sapply(l_seq_100, ml_params, tree = tree_100, Xsim = Xsim_100, 
+                           method = "mvSLOUCH")
+
+# rpf (mvMORPH)
+mlparams_rpf_100 <- sapply(l_seq_100, ml_params, tree = tree_100, Xsim = Xsim_100, 
+                           method = "rpf")
+
+# pic (mvMORPH)
+mlparams_pic_100 <- sapply(l_seq_100, ml_params, tree = tree_100, Xsim = Xsim_100, 
+                           method = "pic")
+
+
+
+
+# ============================= plot the results ===============================
+
+# prepare the colors
+cols <- sapply(c("skyblue1","midnightblue", "orange1"), col2rgb)/255
+col_mvs <- rgb(cols[1,3], cols[2,3], cols[3,3], 0.7)
+col_rpf <- rgb(cols[1,2], cols[2,2], cols[3,2], 0.7)
+col_pic <- rgb(cols[1,1], cols[2,1], cols[3,1], 0.7)
+
+# title for the subplots
+mains <- c(expression(X[0]^{(1)}),
+           expression(X[0]^{(2)}),
+           expression(paste(Sigma, Sigma^T, "[1,1]")),
+           expression(paste(Sigma, Sigma^T, "[1,2]")),
+           expression(paste(Sigma, Sigma^T, "[2,2]")),
+           "max. log-lik.")
+
+# prepare the layout
+Mplots_ <- matrix(1:(4*6), nrow = 6, byrow = TRUE)
+Mplots <- cbind(Mplots_[,1:2], rep(0, length(truevalues)+1), Mplots_[,3:4]) 
+Mlegend <- rep(max(Mplots)+1, 5)
+Mcol <- c(rep(max(Mlegend)+1, 2), 0, rep(max(Mlegend)+2, 2))
+Mrow <- c(0, (max(Mcol)+1):(max(Mcol)+6), 0)
+
+M <- cbind(Mrow, rbind(Mcol, Mplots, Mlegend))
+heights <- c(1, rep(5, 6), 3)
+widths <- c(3, 5,5, 1, 5,5)
+
+
+{
+# ---------------------------- start plot ---------------------------
+
+svg("ML_compare.svg", width = 20, height = 25) 
+ 
+layout(mat = M, heights = heights, widths = widths)
+
+xid <- c(1, seq(20, 100, by = 20))
+i_zoomed <- 15
+xid_zoomed <- c(1, seq(5, i_zoomed, by = 5))
+
+
+ylims_zoomed_4 <- rbind(c(-2, 2),
+                        c(-2, 2),
+                        c(4, 85),
+                        c(-24, 5),
+                        c(1, 11),
+                        c(-17, -10))
+
+ylims_zoomed_100 <- rbind(c(-2, 2),
+                        c(-2, 2),
+                        c(0, 10),
+                        c(0, 10),
+                        c(0, 20),
+                        c(-310, -220))
+
+
+# ---------------------------- plot the results ---------------------------
+par(mar = c(4.1, 4.1, 3.1, 2.1))
+for (i in 1:6){
+  # ------------------------------ plot for n = 4 ------------------------------
+  # ---------------------- zoomed in scale ----------------------
+  plot(mlparams_pic_4[i,1:i_zoomed], pch = 16, col = rgb(0,0,0,0), axes = FALSE, 
+       xlab = "", ylab = "", type = "o", ylim = ylims_zoomed_4[i,])
+  grid()
+  lines(mlparams_pic_4[i,1:i_zoomed], pch = 16, col = col_pic, type = "o", cex = 1.5,
+        lwd = 2)
+  lines(mlparams_rpf_4[i,1:i_zoomed], pch = 5, col = col_rpf, type = "o", cex = 1.5,
+        lty = 2, lwd = 1.5)
+  lines(mlparams_mvs_4[i,1:i_zoomed], pch = 17, col = col_mvs, type = "o", cex = 1.5)
+  
+  axis(1, at = xid_zoomed, labels = formatC(l_seq_4[xid_zoomed], format = "e", digits = 2), 
+       cex.axis = 1.75, line = 1.5)
+  axis(2, cex.axis = 2, line = 1.5)
+  mtext(bquote("\U2113"[i]), side = 1, line = 5, cex = 1.5)
+  abline(h = truevalues[i], lty = 2, lwd = 2)
+  
+  # ---------------------- full scale ----------------------
+  plot(mlparams_pic_4[i,], pch = 16, col = rgb(0,0,0,0), axes = FALSE, 
+       xlab = "", ylab = "", type = "o")
+  grid()
+  lines(mlparams_pic_4[i,], pch = 16, col = col_pic, type = "o", cex = 1.5,
+        lwd = 2)
+  lines(mlparams_rpf_4[i,], pch = 5, col = col_rpf, type = "o", cex = 1.5,
+        lty = 2, lwd = 1.5)
+  lines(mlparams_mvs_4[i,], pch = 17, col = col_mvs, type = "o", cex = 1.5)
+  
+  axis(1, at = xid, labels = formatC(l_seq_4[xid], format = "e", digits = 2), 
+       cex.axis = 1.75, line = 1.5)
+  axis(2, cex.axis = 2, line = 1.5)
+  mtext(bquote("\U2113"[i]), side = 1, line = 5, cex = 1.5)
+  
+  
+  
+  # ----------------------------- plot for n = 100 -----------------------------
+  # ---------------------- zoomed in scale ----------------------
+  plot(mlparams_pic_100[i,1:i_zoomed], pch = 16, col = rgb(0,0,0,0), axes = FALSE, 
+       xlab = "", ylab = "", type = "o", ylim = ylims_zoomed_100[i,])
+  grid()
+  abline(h = truevalues[i], lty = 2, lwd = 2)
+  lines(mlparams_pic_100[i,1:i_zoomed], pch = 16, col = col_pic, type = "o", cex = 1.5,
+        lwd = 2)
+  lines(mlparams_rpf_100[i,1:i_zoomed], pch = 5, col = col_rpf, type = "o", cex = 1.5,
+        lty = 2, lwd = 1.5)
+  lines(mlparams_mvs_100[i,1:i_zoomed], pch = 17, col = col_mvs, type = "o", cex = 1.5)
+  
+  axis(1, at = xid_zoomed, labels = formatC(l_seq_100[xid_zoomed], format = "e", digits = 2), 
+       cex.axis = 1.75, line = 1.5)
+  axis(2, cex.axis = 2, line = 1.5)
+  mtext(bquote("\U2113"[i]), side = 1, line = 5, cex = 1.5)
+  
+  
+  # ---------------------- full scale ----------------------
+  plot(mlparams_pic_100[i,], pch = 16, col = rgb(0,0,0,0), axes = FALSE, 
+       xlab = "", ylab = "", type = "o")
+  grid()
+  lines(mlparams_pic_100[i,], pch = 16, col = col_pic, type = "o", cex = 1.5,
+        lwd = 2)
+  lines(mlparams_rpf_100[i,], pch = 5, col = col_rpf, type = "o", cex = 1.5,
+        lty = 2, lwd = 1.5)
+  lines(mlparams_mvs_100[i,], pch = 17, col = col_mvs, type = "o", cex = 1.5)
+  
+  axis(1, at = xid, labels = formatC(l_seq_100[xid], format = "e", digits = 2), 
+       cex.axis = 1.75, line = 1.5)
+  axis(2, cex.axis = 2, line = 1.5)
+  mtext(bquote("\U2113"[i]), side = 1, line = 5, cex = 1.5)
+  
+}
+
+# ---------------------------- plot legend ---------------------------
+par(mar = c(0,0,0,0))
+plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+legend("center", legend = c("true value", "mvSLOUCH", "mvMORPH (rpf)", "mvMORPH (pic)"),
+       lty = c(2,1,2,1), pch = c(NA, 17, 5, 16), 
+       col = c("black", "orange1", "midnightblue", "skyblue1"), lwd = c(2,1,1,1),
+       horiz = TRUE, x.intersp = 2, text.width = 1.5, cex = 3, bty = "n")
+
+# ---------------------------- plot column names ---------------------------
+plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+text(0,0, paste0("n = 4"), font = 2, cex  = 3)
+
+plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+text(0,0, paste0("n = 100"), font = 2, cex  = 3)
+
+
+# ---------------------------- plot row names ---------------------------
+for (i in 1:6){
+  plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+  text(-1,0, mains[i], font = 1, cex  = 3)
+}
+
+
+dev.off()
+
+}
+
+
+# =================== check ML estimates ===================
+# minimum length of l for mvSLOUCH:
+l_seq_4[max(which(!is.na(mlparams_mvs_4[6,])))]
+l_seq_100[max(which(!is.na(mlparams_mvs_100[6,])))]
+
+# parameter values
+mlparams_mvs_4[,max(which(!is.na(mlparams_mvs_4[6,])))]
+mlparams_mvs_100[,max(which(!is.na(mlparams_mvs_100[6,])))]
+
+# # minimum length of l for rpf:
+l_seq_4[tail(which(!is.na(mlparams_rpf_4[6,])), 2)]
+mlparams_rpf_4[,max(which(!is.na(mlparams_rpf_4[6,])))]
+
+# # minimum length of l for pic:
+l_seq_100[max(which(!is.na(mlparams_pic_100[6,])))]
+mlparams_pic_4[,100]
+mlparams_pic_100[,dim(mlparams_pic_100)[2]]
+
+
+
+
+
---
+++ short_tip_remove.R
@@ -0,0 +1,263 @@
+# ----- compare the ML results from different new trait assignments ------------
+# ------ on the pruned tree ----------------------------------------------------
+# experiment parameters:
+# - Number of tips (n): 5, 25, 50, 100
+# - number of simulations: 100
+# - Trait assignment: average, left, right
+
+library(ape)
+library(TreeSim)
+library(phytools)
+library(mvSLOUCH)
+library(mvMORPH)
+
+# random seed 
+random_seeds <- 1234 + 0:4
+
+# ========================= generate a list of trees =========================
+n_tips <- c(5, 25, 50, 100)
+n_sim <- 100
+l <- 1e-5 # close to the threshold of mvSLOUCH
+
+set.seed(random_seeds[1])
+TREES <- lapply(n_tips, function(n){
+  TreeSim::sim.bd.taxa(n, numbsim = n_sim, lambda = 1, mu = 0)})
+
+
+# =========== convert trees into trees with short tips =========================
+short_tip_tree <- function(tree, l = 1e-5){
+  # normalize the height of the tree into unit height
+  tree$edge.length <- tree$edge.length/max(phytools::nodeHeights(tree))
+  
+  # find which tip branch is the shortest
+  tip_id <- which(tree$edge[,2] %in% 1:Ntip(tree))
+  edge_id <- tip_id[which.min(tree$edge.length[tip_id])]
+  parent_node <- tree$edge[edge_id,1]
+  ids_desc <- which(tree$edge[,1] == parent_node)
+  id_anc <- which(tree$edge[,2] == parent_node)
+  
+  # change the values: shorten tip branches, lengthen parent branch
+  l_orig <- tree$edge.length[ids_desc][1]
+  tree$edge.length[ids_desc] <- l
+  tree$edge.length[id_anc] <- tree$edge.length[id_anc] + l_orig - l
+  
+  return(tree)
+}
+
+# ------------ trees with short tip branch -----> use this trees
+TREES_st <- list()
+for (i in 1:length(n_tips)){
+  TREES_st[[i]] <- lapply(TREES[[i]], short_tip_tree)
+}
+
+
+# -------------- retrieve labels of the short tips
+get_st_labs <- function(tree, l = 1e-5){
+  st_id <- tree$edge[which(tree$edge.length == l),2]
+  st_labels <- tree$tip.label[st_id]
+  
+  return(setNames(c(st_labels, st_id),
+                  c("tip1", "tip2", "id1", "id2")))
+}
+
+# retrieve labels
+st_labs <- list()
+for (i in 1:length(n_tips)){
+  st_labs[[i]] <- t(sapply(TREES_st[[i]], get_st_labs))
+}
+
+
+
+# =========== prune one of the two short tip branches in the tree ==============
+# run ML fit on pruned trees
+# prune the short tip branch
+prTREES <- list()
+
+# ---> the lower index of the two tips is removed
+for (i in 1:length(n_tips)){
+  prTREES[[i]] <- lapply(1:n_sim, function(j){drop.tip(TREES_st[[i]][[j]], 
+                                                       as.numeric(st_labs[[i]][j,3]))})
+}
+
+
+
+
+# ============= simulate data for each tree (with short tips) ==================
+# true parameters:
+X0 <- matrix(c(0, 0), ncol = 1) # ancestral trait value at the root
+Sigma <- matrix(c(1,2,2,1), nrow = 2) # BM diffusion
+StS <- Sigma %*% t(Sigma)
+truevalues <- c(X0, c(StS)[c(1,2,4)])
+
+# simulate data at the tips
+X_SIM <- list()
+for (i in 1:length(n_tips)){
+  set.seed(random_seeds[i+1])
+  X_SIM[[i]] <- lapply(TREES_st[[i]], 
+                       function(tree){simulBMProcPhylTree(phyltree = tree, 
+                                                          X0 = X0, 
+                                                          Sigma = Sigma)})
+}
+
+
+# ------------ modify the simulated data to fit the pruned tree
+mod_X <- function(X, tip_labs, method = c("avg", "left", "right")){
+  traits_tips <- X[tip_labs[1:2],]
+  
+  if (method == "avg"){
+    new_traits <- colMeans(traits_tips)
+  } else if (method == "left"){
+    new_traits <- traits_tips[1,]
+  } else if (method == "right"){
+    new_traits <- traits_tips[2,]
+  }
+  
+  # remove the trait of left tip in X, 
+  # replace by new_traits
+  X_new <- X[-which(rownames(X) == tip_labs[1]),]
+  X_new[tip_labs[2],] <- new_traits
+  
+  return(X_new)
+}
+
+
+# case 1: average of the two tips
+X_SIM_avg <- list() 
+
+# case 2: left of the two tips
+X_SIM_left <- list() 
+
+# case 3: right of the two tips
+X_SIM_right <- list() 
+
+
+for (i in 1:length(n_tips)){
+  X_SIM_avg[[i]] <- lapply(1:n_sim, 
+                           function(j){mod_X(X = X_SIM[[i]][[j]], 
+                                             tip_labs = st_labs[[i]][j,],
+                                             method = "avg")}) 
+  
+  X_SIM_left[[i]] <- lapply(1:n_sim, 
+                            function(j){mod_X(X = X_SIM[[i]][[j]], 
+                                              tip_labs = st_labs[[i]][j,],
+                                              method = "left")}) 
+  
+  X_SIM_right[[i]] <- lapply(1:n_sim, 
+                             function(j){mod_X(X = X_SIM[[i]][[j]], 
+                                               tip_labs = st_labs[[i]][j,],
+                                               method = "right")}) 
+}
+
+
+
+# ===== find ML parameters for the pruned trees based on different cases =======
+
+# function to run ML fit given a tree and simulated data
+# returns a vector of X0's, StS values, and max log-lik
+MLparams <- function(tree, Xsim){
+  res <- mvSLOUCH::BrownianMotionModel(phyltree = tree, mData = Xsim)
+  return(c(c(res$ParamsInModel$vX0), c(res$ParamSummary$StS)[c(1,2,4)],
+           res$ParamSummary$LogLik))
+}
+
+
+# list for the pruned tree
+MLavg <- list()
+MLleft <- list()
+MLright <- list()
+
+for (i in 1:length(n_tips)){
+  # average of the two short branch tips
+  MLavg[[i]] <- t(sapply(1:n_sim, function(j){MLparams(prTREES[[i]][[j]], 
+                                                       X_SIM_avg[[i]][[j]])}))
+  
+  # left of the two short branch tips
+  MLleft[[i]] <- t(sapply(1:n_sim, function(j){MLparams(prTREES[[i]][[j]], 
+                                                        X_SIM_left[[i]][[j]])}))
+  
+  # right of the two short branch tips
+  MLright[[i]] <- t(sapply(1:n_sim, function(j){MLparams(prTREES[[i]][[j]], 
+                                                         X_SIM_right[[i]][[j]])}))
+  
+  # print the progress
+  print(paste0("n_tips: ", n_tips[i]))
+}
+
+
+# save the ML results
+# save(MLavg, file = "MLavg.RData")
+# save(MLleft, file = "MLleft.RData")
+# save(MLright, file = "MLright.RData")
+
+
+
+# ============================ plot the results ================================
+
+xlims <- rbind(c(-5,5),
+               c(-5,5),
+               c(0,10),
+               c(-10,0),
+               c(0,10))
+
+cols <- c(rgb(1,0.1,0.1,0.4),
+          rgb(0.1,1,0.1,0.4),
+          rgb(0.1,0.1,1,0.4))
+
+mains <- c(expression(X[0]^{(1)}),
+           expression(X[0]^{(2)}),
+           expression(paste(Sigma, Sigma^T, "[1,1]")),
+           expression(paste(Sigma, Sigma^T, "[1,2]")),
+           expression(paste(Sigma, Sigma^T, "[2,2]")))
+
+Mplots <- rbind(matrix(1:20, nrow = 4), rep(21, 5))
+Mrow <- c(0, max(Mplots) + 1:4, 0)
+Mcol <- max(Mrow) + 1:5
+
+
+{
+
+pdf(file = "removed_tip.pdf", width = 16, height = 10)
+layout(cbind(Mrow, rbind(Mcol, Mplots)), 
+       heights = c(1,rep(5,4),1), widths = c(2, rep(5,5)))
+
+# -------------------------- boxplots --------------------------
+par(mar = c(1.1, 2.1, 1.1, 2.1))
+for (i in 1:length(truevalues)){
+  for (j in 1:length(n_tips)){
+    df1 <- cbind(MLavg[[j]][,i], MLleft[[j]][,i], MLright[[j]][,i])
+    boxplot(df1, col = rgb(0,0,0,0), border = NA, xaxt = "n",
+            cex.axis = 1.5)
+    grid()
+    boxplot(df1, col = c("dodgerblue4", "skyblue2", "lightcyan"), add = TRUE,
+            boxwex = 0.5,  xaxt = "n", yaxt = "n")
+    abline(h = truevalues[i], lwd = 3, col = "red", lty = 2)
+  }
+}
+
+# -------------------------- legend --------------------------
+
+par(mar = c(0,0,0,0))
+plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+legend("center", legend = c("true value", "average", "left tip", "right tip"),
+       lty = c(2,rep(NA, 3)), pch = c(NA, rep(15, 3)), col = c("red",rep(NA,3)), 
+       lwd = rep(2,4), horiz = TRUE, x.intersp = 1, text.width = 1.5, cex = 2, bty = "n",
+       border = c(NA, rep("black", 3)), 
+       fill = c(NA, "dodgerblue4", "skyblue2", "lightcyan"))
+
+
+# -------------------------- row names (n_tips) --------------------------
+for (i in 1:length(n_tips)){
+  plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+  text(0,0, paste0("n = ", n_tips[i]), font = 2, cex  = 1.5)
+}
+
+
+# -------------------------- row names (param. names) --------------------------
+for (i in 1:length(truevalues)){
+  plot(NA, xlim = c(-5,5), ylim = c(-5,5), axes = FALSE, xlab = "", ylab = "")
+  text(0,0, mains[i], cex  = 1.5)
+}
+
+dev.off()
+
+}
