ebd3da9b9fecd78b42982a2f9dc95f2361e6c3dd
krzbar <bartoszekkj@gmail.com>
2025-04-22 17:38:43 +0200

    Header

--- short_tip_remove.R
+++ short_tip_remove.R
@@ -1,3 +1,15 @@
+## These are the R scripts and numerical results accompanying 
+## Bartoszek, Brahmantio, Munoz-Duran, Fuentes-Gonzalez,Pienaar, and Polly
+## Short branch singularities in phylogenetic comparative methods
+
+## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
+## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+## Please understand that there may still be bugs and errors. Use it at your own risk. 
+## We take no responsibility for any errors or omissions in this code or for any misfortune 
+## that may befall you or others as a result of its use. Please send comments and report 
+## bugs to Krzysztof Bartoszek at krzbar@protonmail.ch .
+
+
 # ----- compare the ML results from different new trait assignments ------------
 # ------ on the pruned tree ----------------------------------------------------
 # experiment parameters:
